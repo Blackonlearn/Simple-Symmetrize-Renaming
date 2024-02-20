24ad58400d59fb8bf9da363cd6171e46f1d85007
BongQi <42308380+Blackonlearn@users.noreply.github.com>
2024-02-20 10:54:07 +0700

    Create Simple Symmetrize Renaming.py

---
+++ Simple Symmetrize Renaming.py
@@ -0,0 +1,24 @@
+import bpy
+
+C = bpy.context
+D = bpy.data
+
+def symmetrizeName():
+    L_Name = C.active_bone
+    for i,j in enumerate(C.selected_pose_bones):
+        if j.name == L_Name.name:
+            pass
+        else:
+            a = f"{L_Name.name}"
+            if ".L" in a:
+                R_Name = a.replace(".L",".R")
+            elif ".R" in a:
+                R_Name = a.replace(".R",".L")
+            elif ".F" in a:
+                R_Name = a.replace(".F",".B")
+            elif ".B" in a:
+                R_Name = a.replace(".B",".F")
+            print(R_Name)
+            j.name = R_Name
+        
+symmetrizeName()
